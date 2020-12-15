80b09505ac1bef7d5d7cf4e932b52efbc173fc22
Amelia Bleeker <amelia.bleeker@pnnl.gov>
2020-12-15 12:56:24 -0800

    Added ability to disable the request account functionality. Updated the readme to include more detailed information about logging as well as account management.

--- server/index.js
+++ server/index.js
@@ -98,7 +98,7 @@ const desc = {
     winston.format.prettyPrint()
   ),
   meta: false, // optional: control whether you want to log the meta data about the request (default to true)
-  msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+  msg: "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
   expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
   colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
   ignoreRoute: function(req, res) {
